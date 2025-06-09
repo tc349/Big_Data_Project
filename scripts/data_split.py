2d31d471ac4c80a57cb4930492d5498c84ed7e6f
haseeb <rhashraf@live.com>
2025-06-09 14:51:05 +0100

    new python code and new three files

--- scripts/data_split.py
+++ scripts/data_split.py
@@ -8,21 +8,25 @@ os.makedirs(output_dir, exist_ok=True)
 
 
 df = pd.read_csv(input_file)
+total_rows = len(df)
 
 # Validate length
-if len(df) != 50000:
-    raise ValueError(f"Expected 50,000 rows, found {len(df)} rows.")
+full_load_end = int(total_rows * 0.60)
+incremental_end = full_load_end + int(total_rows * 0.20)
 
 
-df_batch = df.iloc[:30000]
-df_incremental = df.iloc[30000:40000]
-df_kafka = df.iloc[40000:]
+df_batch = df.iloc[:full_load_end]
+df_incremental = df.iloc[full_load_end:incremental_end]
+df_kafka = df.iloc[incremental_end:]
 
 
 df_batch.to_csv(f'{output_dir}/full_load.csv', index=False)
 df_incremental.to_csv(f'{output_dir}/incremental_load.csv', index=False)
 df_kafka.to_csv(f'{output_dir}/kafka_streaming.csv', index=False)
 
-print("CSV successfully split and saved to data/split/")
+print(f" Split complete:")
+print(f" - Full Load (60%): {len(df_batch)} rows")
+print(f" - Incremental (20%): {len(df_incremental)} rows")
+print(f" - Kafka (20%): {len(df_kafka)} rows")
 
 
